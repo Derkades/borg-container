272f7e76dab47e2bc802c90a01db105272865818
Robin Slot <robin@rslot.nl>
2026-01-22 11:43:53 +0100

    clean up manifest

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 set -e
+! podman manifest exists borg-serve || podman manifest rm borg-serve
 podman manifest create -a borg-serve
 podman build --platform=linux/arm64,linux/amd64 --manifest borg-serve .
 podman manifest push --all borg-serve docker://docker.io/derkades/borg-serve
+podman manifest rm borg-serve
